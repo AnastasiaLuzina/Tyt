ae685600da3906946b9db04ef1eeb70457cbffbd
NechistaySila <hodzobuldag@mail.ru>
2026-06-06 10:02:25 +0300

    refactor: замена компонента TrustDots на функцию renderTrustDots

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,16 +1,8 @@
-import React from 'react';
-
-// Компонент для отображения точек доверия (уровень от 0 до 5)
-export const TrustDots = ({ level }) => {
-  // level – число, сколько точек должно быть активным
-  return (
-    <div className="trust-level">
-      {[...Array(5)].map((_, i) => (
-        <div
-          key={i}
-          className={`trust-dot ${i < level ? 'active' : ''}`}
-        />
-      ))}
-    </div>
-  );
-};
+export function renderTrustDots(level) {
+  let html = '<div class="trust-level">';
+  for (let i = 0; i < 5; i++) {
+    html += `<div class="trust-dot ${i < level ? 'active' : ''}"></div>`;
+  }
+  html += '</div>';
+  return html;
+}
